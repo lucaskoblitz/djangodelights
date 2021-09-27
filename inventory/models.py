228907499203832b1f5ad5693124bb12422142ac
lucas <lcalandino@gmail.com>
2021-09-27 09:04:31 -0300

    make models

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+	
+class Ingredient(models.Model):
+	name = models.CharField(max_length=35)
+	quantity = models.IntegerField()
+	unit = models.CharField(max_length=5)
+	unit_price = models.FloatField()
+
+	def __str__(self):
+		return self.name
+
+class MenuItem(models.Model):
+	title = models.CharField(max_length=35)
+	price = models.FloatField()
+	example = models.ImageField()
+
+	def __str__(self):
+		return self.title
+
+class RecipeRequirement(models.Model):
+	menu_items = models.ForeignKey(MenuItem, default = 1,  on_delete=models.SET_DEFAULT)
+	ingredient = models.CharField(max_length=35)
+	quantity = models.FloatField()
+
+	def __str__(self):
+		return str(self.menu_items)
+
+
+class Purchase(models.Model):
+	menu_item = models.ForeignKey(MenuItem, default=1, on_delete=models.SET_DEFAULT)
+	timestamp = models.DateTimeField()
+
+	def __str__(self):
+		return 'Purchase ' + str(self.menu_item) + str(self.timestamp)
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Ingredient 
 # Create your views here.
+
+def home(request):
+	return render(request, 'inventory/home.html')
+
+class Inventory(ListView):
+	model = Ingredient
+	template_name = 'inventory/inventory_list.html'
